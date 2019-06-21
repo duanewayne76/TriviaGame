eda2cbfc0dfa5c3ee19d8991ae936b97f865f519
Duane Stafford <duanestafford@Duanes-MBP.richmond.edu>
2019-06-20 22:04:41 -0400

    bug fixes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,32 @@
 
 //Disclaimer: I had to use an online tutorial in hopes to be able to understand more about the process...I'm lost and yes, I have a tutor.
 
-$('#start').on('click', function () {
 
-    game.start();
-
-})
 
 var questions = [
     {
         question: "Which 80s movie was the highest grossing film of the decade?",
-        answers: ["A Bug's Life", "Monsters Inc", "Toy Story", "E.T. The Extraterrestrial"],
+        answers: ["Die Hard", "Flashdance", "Police Academy", "E.T. The Extraterrestrial"],
         correctAnswer: "E.T. The Extraterrestrial"
     },
     {
         question: "Which 80s movie was Alan Rickman’s first feature film role?",
-        answers: ["Die Hard", "Monsters Inc", "Toy Story", "The Lion King"],
+        answers: ["Die Hard", "Flashdance", "Police Academy", "E.T. The Extraterrestrialg"],
         correctAnswer: "Die Hard"
     },
     {
         question: "Which 80s movie spawned 6 sequels and a T.V. series?",
-        answers: ["A Bug's Life", "Monsters Inc", "Police Academy", "The Lion King"],
+        answers: ["Die Hard", "Flashdancec", "Police Academy", "E.T. The Extraterrestrial"],
         correctAnswer: "Police Academy"
     },
     {
         question: "Which 80’s movie was the first to become a hit largely due to MTV",
-        answers: ["A Bug's Life", "Flashdance", "Toy Story", "The Lion King"],
+        answers: ["Die Hard", "Flashdance", "Police Academy", "E.T. The Extraterrestrial"],
         correctAnswer: "Flashdance"
     },
     {
         question: "What artist sang the 1986 song “Danger Zone” from Top Gun?",
-        answers: ["A Bug's Life", "Kenny Loggin", "Toy Story", "The Lion King"],
+        answers: ["Tiffany", "Kenny Loggin", "Toy Story", "The Lion King"],
         correctAnswer: "Kenny Loggins"
     },
     {
@@ -60,10 +56,16 @@ var questions = [
     },
     {
         question: 'Which 80s animated movie’s tagline was "Beyond good. Beyond evil. Beyond your wildest imagination."?',
-        answers: ["A Bug's Life", "The Transformers: The Movie", "Toy Story", "The Lion King"],
+        answers: ["The Secret of NIMH", "The Transformers: The Movie", "Toy Story", "The Lion King"],
         correctAnswer: "The Transformers: The Movie"
 
     }];
+
+$('#start').on('click', function () {
+
+    game.start();
+
+})
 
 var game = {
     correct: 0,
@@ -80,8 +82,9 @@ var game = {
     },
     start: function () {
         timer = setInterval(game.countdown, 1000);
-        $('subwrapper').prepend('<h2>Time Remaining: <span id="counter"</span> Seconds</h2>');
+        $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter"</span> Seconds</h2>');
         $('#start').remove();
+
         //Display questions and potential answers
         for (i = 0; i < questions.length; i++) {
             $('#subwrapper').append('<h2>' + questions[i].question + '</h2>');
@@ -96,79 +99,87 @@ var game = {
         }
 
         $('#subwrapper').append('<br><button id="end">Done</button>');
+
+        $(document).on("click", "#end", function () {
+            game.done();
+        })
+
+
+
+
     },
-        done: function () {
-        $.each($('input[name="question-0]":checked')), function () {
+    done: function () {
+        $.each($('input[name="question-0"]:checked'), function () {
             if ($(this).val() == questions[0].correctAnswer) {
                 game.correct++;
             } else {
                 game.incorrect++;
             }
-        }
+        })
 
-        $.each($('input[name="question-1]":checked')), function () {
+        $.each($('input[name="question-1"]:checked'), function () {
             if ($(this).val() == questions[1].correctAnswer) {
                 game.correct++;
             } else {
                 game.incorrect++;
             }
-        };
-        $.each($('input[name="question-2]":checked')), function () {
+        })
+        $.each($('input[name="question-2"]:checked'), function () {
             if ($(this).val() == questions[2].correctAnswer) {
                 game.correct++;
             } else {
                 game.incorrect++;
             }
-        };
-        $.each($('input[name="question-3]":checked')), function () {
+        })
+        $.each($('input[name="question-3"]:checked'), function () {
             if ($(this).val() == questions[3].correctAnswer) {
                 game.correct++;
             } else {
                 game.incorrect++;
             }
-        };
-        $.each($('input[name="question-4]":checked')), function () {
+        })
+        $.each($('input[name="question-4"]:checked'), function () {
             if ($(this).val() == questions[4].correctAnswer) {
                 game.correct++;
             } else {
                 game.incorrect++;
             }
-        };
-        $.each($('input[name="question-5]":checked')), function () {
+        })
+        $.each($('input[name="question-5"]:checked'), function () {
             if ($(this).val() == questions[5].correctAnswer) {
                 game.correct++;
             } else {
                 game.incorrect++;
             }
-        };
-        $.each($('input[name="question-6]":checked')), function () {
+        })
+        $.each($('input[name="question-6"]:checked'), function () {
             if ($(this).val() == questions[6].correctAnswer) {
                 game.correct++;
             } else {
                 game.incorrect++;
             }
-        };
-        $.each($('input[name="question-7]":checked')), function () {
+        })
+        $.each($('input[name="question-7"]:checked'), function () {
             if ($(this).val() == questions[7].correctAnswer) {
                 game.correct++;
             } else {
                 game.incorrect++;
             }
-        };
-        $.each($('input[name="question-8]":checked')), function () {
+        })
+        $.each($('input[name="question-8"]:checked'), function () {
             if ($(this).val() == questions[8].correctAnswer) {
                 game.correct++;
             } else {
                 game.incorrect++;
             }
-        };
-        $.each($('input[name="question-9]":checked')), function () {
+        })
+        $.each($('input[name="question-9"]:checked'), function () {
             if ($(this).val() == questions[9].correctAnswer) {
                 game.correct++;
             } else {
                 game.incorrect++;
             }
-        };
+        })
 
         this.result();
     },
@@ -177,9 +188,9 @@ var game = {
         clearInterval(timer);
         $('#subwrapper h2').remove();
         $('#subwrapper').html("<h2>All done!</h2>");
-        $('subwrapper').append("<h3>Correct Answers: " + this.correct + "</h3>");
-        $('subwrapper').append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
-        $('subwrapper').append("<h3>Unanswered: " + (questions.length - (this.correct = this.incorrect)) + "</h3>");
+        $('#subwrapper').append("<h3>Correct Answers: " + this.correct + "</h3>");
+        $('#subwrapper').append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+        $('#subwrapper').append("<h3>Unanswered: " + (questions.length - (this.correct + this.incorrect)) + "</h3>");
     }
 
 
@@ -193,6 +204,6 @@ var game = {
 //         clearInterval(timerId);
 //     }
 //     $("#timer").text(time);
-
+//$("#timer").text(timer);
 // }, 1000);
 //
